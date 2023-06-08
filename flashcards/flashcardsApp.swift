9c1a0c4a955e4f1b3eb16ccfef2ebd7d90ec4293
Dan Morain <dan.morain91@gmail.com>
2023-06-08 18:18:47 -0400

    Initial Commit

---
+++ flashcards/flashcardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  flashcardsApp.swift
+//  flashcards
+//
+//  Created by Dan Morain on 6/8/23.
+//
+
+import SwiftUI
+
+@main
+struct flashcardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
